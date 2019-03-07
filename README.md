e624c8291556d6a344c3efa5fdd1941a5ac7460b
Alex Baranowski <alex@euro-linux.com>
2019-03-07 17:54:11 +0100

    Add shortcuts

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bash Shortcuts Tearcher
+
+Currently supported shortcuts
+
+## Automatic installation
+
+## Usage
+You can copy file
+
---
+++ bash-shortucts-teacher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bind '"\e[A":"BT: Did you mean CTRL + p?"'
+bind '"\e[B":"BT: Did you mean CTRL + n?"'
+bind '"\e[C":"BT: Did you mean CTRL + f or ALT + f?"'
+bind '"\e[D":"BT: Did you mean CTRL + b or ALT + b??"'
+bind '"\e[H":"BT: Did you mean CTRL + a?"'
+bind '"\e[F":"BT: Did you mean CTRL + e?"'
